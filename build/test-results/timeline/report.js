829820b309ff8be5faae39bd961a849a4592965e
brandon5254 <83289428+brandon5254@users.noreply.github.com>
2023-11-24 04:09:48 -0500

    .

--- build/test-results/timeline/report.js
+++ build/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"a25e0203-0154-4a6b-964c-451329f3cbd0","feature":"Agregar nuevo elemento a la lista de tareas","scenario":"Agregar \"Terminar Trabajo de progamacion\" a la lista de tareas","start":1700805715127,"group":28,"content":"","tags":"@prueba,","end":1700805721253,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"43d08713-d182-417f-9d65-9102a1428c2c","feature":"Agregar nuevo elemento a la lista de tareas","scenario":"Agregar \"Terminar Trabajo de progamacion\" a la lista de tareas","start":1700809261875,"group":28,"content":"","tags":"@prueba,","end":1700809269942,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":28,"content":"Thread[ForkJoinPool-2-worker-1,5,main]"}]);
 });
